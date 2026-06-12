8f23b346cc79de4d34e162527c484586b3fcb89b
Enoblk <58480254+Enoblk@users.noreply.github.com>
2026-06-12 07:14:47 -0700

    Stabilize grill controller firmware

--- src/Utility.h
+++ src/Utility.h
@@ -1,30 +1,22 @@
-// Utility.h - Minimal header for simple temperature reading
 #ifndef UTILITY_H
 #define UTILITY_H
 
 #include <Arduino.h>
 
-// Debug control flags
 extern bool debugGrillSensor;
-extern bool debugAmbientSensor; 
+extern bool debugAmbientSensor;
 extern bool debugMeatProbes;
 extern bool debugRelays;
 extern bool debugSystem;
 
-// Temperature functions
-double readTemperature();              
-double readGrillTemperature();         
-double readAmbientTemperature();       
+double readTemperature();
+double readGrillTemperature();
+double readAmbientTemperature();
 String getStatus(double temp);
 bool isValidTemperature(double temp);
+bool grillTemperatureHealthy();
+unsigned long grillTemperatureAgeMs();
 
-
-
-void testSpecificProbe();
-void testAmbientSensor();
-
-
-// Debug control functions
 void setGrillDebug(bool enabled);
 void setAmbientDebug(bool enabled);
 void setMeatProbesDebug(bool enabled);
@@ -32,23 +24,17 @@ void setRelayDebug(bool enabled);
 void setSystemDebug(bool enabled);
 void setAllDebug(bool enabled);
 
-// Status functions
 bool getGrillDebug();
 bool getAmbientDebug();
 bool getMeatProbesDebug();
 bool getRelayDebug();
 bool getSystemDebug();
 
-// Simple calibration system
 void setupTemperatureCalibration();
 void printCalibrationStatus();
 void handleCalibrationCommands(String command);
-
-// Diagnostic functions
 void runTemperatureDiagnostics();
 void testGrillSensor();
-
-// Compatibility functions
 void resetCalibration();
 void saveCalibrationData();
 void loadCalibrationData();
